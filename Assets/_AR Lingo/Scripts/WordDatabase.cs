2fbc4ba6197e2bc271dbf2b44545c5680dbb2138
Sleeplessmen <124755138+Sleeplessmen@users.noreply.github.com>
2025-12-12 13:43:42 +0700

    adding simple nature package and play with project a bit

--- Assets/_AR Lingo/Scripts/WordDatabase.cs
+++ Assets/_AR Lingo/Scripts/WordDatabase.cs
@@ -1,7 +1,23 @@
+using System.Collections.Generic;
 using UnityEngine;
+using System.Linq; // Dùng LINQ để tìm kiếm nhanh
 
-[CreateAssetMenu(fileName = "WordDatabase", menuName = "Scriptable Objects/WordDatabase")]
+[CreateAssetMenu(fileName = "WordDatabase", menuName = "AR Lingo/Database")]
 public class WordDatabase : ScriptableObject
 {
-    
+    [SerializeField] private List<WordData> allWords;
+
+    // Hàm tìm kiếm con vật dựa trên ID (Dùng khi AR Camera scan được target)
+    public WordData GetWordByID(string searchID)
+    {
+        // Tìm trong list xem có con nào trùng ID không
+        return allWords.FirstOrDefault(w => w.id == searchID);
+    }
+
+    // Hàm lấy danh sách ngẫu nhiên để làm Quiz (Sprint sau)
+    public List<WordData> GetRandomWords(int count)
+    {
+        // Shuffle và lấy count phần tử (Logic giả định, cần viết kỹ hơn sau này)
+        return allWords.OrderBy(x => Random.value).Take(count).ToList();
+    }
 }
